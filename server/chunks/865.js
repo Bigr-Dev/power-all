32d7685b8cd0dcdc5ad14f4be89e6f752b74713f
Bigr-Dev <rendani.ramaphosa@gmail.com>
2024-12-17 10:39:40 +0200

    test

---
+++ server/chunks/865.js
@@ -0,0 +1,112 @@
+exports.id=865,exports.ids=[865],exports.modules={8303:e=>{function s(e){var s=Error("Cannot find module '"+e+"'");throw s.code="MODULE_NOT_FOUND",s}s.keys=()=>[],s.resolve=s,s.id=8303,e.exports=s},3685:(e,s,r)=>{Promise.resolve().then(r.t.bind(r,3219,23)),Promise.resolve().then(r.t.bind(r,4863,23)),Promise.resolve().then(r.t.bind(r,5155,23)),Promise.resolve().then(r.t.bind(r,9350,23)),Promise.resolve().then(r.t.bind(r,6313,23)),Promise.resolve().then(r.t.bind(r,8530,23)),Promise.resolve().then(r.t.bind(r,8921,23))},6837:(e,s,r)=>{Promise.resolve().then(r.t.bind(r,6959,23)),Promise.resolve().then(r.t.bind(r,3875,23)),Promise.resolve().then(r.t.bind(r,8903,23)),Promise.resolve().then(r.t.bind(r,4178,23)),Promise.resolve().then(r.t.bind(r,6013,23)),Promise.resolve().then(r.t.bind(r,7190,23)),Promise.resolve().then(r.t.bind(r,1365,23))},3615:(e,s,r)=>{Promise.resolve().then(r.bind(r,9516))},1863:(e,s,r)=>{Promise.resolve().then(r.bind(r,2392))},8581:(e,s,r)=>{Promise.resolve().then(r.bind(r,9097))},2149:(e,s,r)=>{Promise.resolve().then(r.bind(r,1264))},6487:()=>{},8335:()=>{},3864:(e,s,r)=>{"use strict";Object.defineProperty(s,"__esModule",{value:!0}),function(e,s){for(var r in s)Object.defineProperty(e,r,{enumerable:!0,get:s[r]})}(s,{default:function(){return c},getImageProps:function(){return o}});let t=r(5488),i=r(2034),a=r(1902),n=t._(r(1628));function o(e){let{props:s}=(0,i.getImgProps)(e,{defaultLoader:n.default,imgConf:{deviceSizes:[640,750,828,1080,1200,1920,2048,3840],imageSizes:[16,32,48,64,96,128,256,384],path:"/_next/image",loader:"default",dangerouslyAllowSVG:!1,unoptimized:!1}});for(let[e,r]of Object.entries(s))void 0===r&&delete s[e];return{props:s}}let c=a.Image},2392:(e,s,r)=>{"use strict";r.d(s,{default:()=>l});var t=r(5512),i=r(7137),a=r(8260),n=r(7908),o=r(6008),c=r(9334);let l=()=>{let e=(0,c.usePathname)();return(0,t.jsx)("div",{className:"/"===e?"container mx-auto px-1 sticky":"  px-6 sticky",children:(0,t.jsxs)("div",{className:"flex justify-between items-center h-16",children:[(0,t.jsx)(o.default,{href:"/",className:" hover:text-yellow-500  transition",children:(0,t.jsxs)("div",{className:"flex items-center space-x-2",children:["/"!==e?(0,t.jsx)(i.A,{className:"h-5 w-5 text-yellow-500"}):(0,t.jsx)(a.A,{className:"h-8 w-8 text-yellow-500 "}),(0,t.jsx)("span",{className:"text-1xl font-bold",children:"/"===e?"Power All":"Home"})]})}),(0,t.jsx)("nav",{className:"hidden md:flex space-x-8",children:[{id:"1",name:"Solar"},{id:"2",name:"Generators"},{id:"3",name:"Fabrication"}].map(s=>{let r=e.startsWith(`/services/${s.id}`);return(0,t.jsx)(o.default,{href:`/services/${s.id}`,className:`${r&&"text-yellow-500"} hover:text-yellow-500  transition`,replace:!0,children:s.name},s.id)})}),"/"===e?(0,t.jsxs)("div",{className:"flex items-center space-x-2 hover:text-yellow-500 transition",children:[(0,t.jsx)(n.A,{className:"h-5 w-5 text-yellow-500"}),(0,t.jsx)("span",{className:"hidden md:block",children:"Partner Portal"})]}):(0,t.jsxs)("div",{className:"flex items-center space-x-2",children:[(0,t.jsx)(a.A,{className:"h-5 w-5 text-yellow-500 "}),(0,t.jsx)("span",{className:"text-1xl font-bold",children:"Power All"})]})]})})}},1264:(e,s,r)=>{"use strict";r.d(s,{default:()=>d});var t=r(5512);let i={src:"/_next/static/media/small_business2.8de433ce.jpg",height:4e3,width:6e3,blurDataURL:"data:image/jpeg;base64,/9j/2wBDAAoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/v/2wBDAQoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/v/wgARCAAFAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAT/xAAVAQEBAAAAAAAAAAAAAAAAAAAAAv/aAAwDAQACEAMQAAAAmEv/xAAeEAAABAcAAAAAAAAAAAAAAAAAAgQFAQMREhQzQv/aAAgBAQABPwAj25xWKJ2Sel2vkf/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z",blurWidth:8,blurHeight:5};var a=r(8009);let n=({isOpen:e,onClose:s,children:r})=>e?(0,t.jsx)("div",{id:"wrapper",className:"fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center backdrop-blur-sm",onClick:e=>{"wrapper"===e.target.id&&s()},children:(0,t.jsxs)("div",{className:"bg-white p-6 rounded-lg max-w-2xl w-full mx-4",children:[(0,t.jsx)("div",{className:"flex justify-end",children:(0,t.jsx)("button",{onClick:s,className:"text-gray-500 hover:text-gray-700",children:(0,t.jsx)("svg",{className:"h-6 w-6",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",children:(0,t.jsx)("path",{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:2,d:"M6 18L18 6M6 6l12 12"})})})}),r]})}):null;var o=r(3864),c=r.n(o);let l=({product:e,onClick:s})=>(0,t.jsxs)("div",{className:"bg-gray-50 rounded-lg shadow-lg overflow-hidden cursor-pointer transition-transform duration-300 hover:scale-105",onClick:s,children:[(0,t.jsx)("h2",{className:"text-yellow-600 text-2xl font-bold text-center mb-2 pt-2",children:e.brand_name}),(0,t.jsx)(c(),{src:e.image||i,alt:e.name,className:"bg-white w-full h-48 object-cover"}),(0,t.jsx)("h3",{className:"text-gray-700 text-1xl font-bold text-center mb-1 pt-1",children:e.name}),(0,t.jsx)("div",{className:"p-2 ",children:(0,t.jsx)("div",{className:"overflow-x-auto",children:(0,t.jsx)("p",{className:"text-center",children:e.description})})})]}),d=({products:e})=>{let[s,r]=(0,a.useState)(1),[o,d]=(0,a.useState)(null),A=9*s,m=e.slice(A-9,A),p=e=>{d(e)};return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("div",{className:" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  gap-6 mt-6",children:m.map((e,s)=>(0,t.jsx)(l,{product:e,onClick:()=>p(e)},s))}),(0,t.jsxs)("div",{className:"flex justify-center mt-10",children:[(0,t.jsx)("button",{onClick:()=>{s>1&&r(s-1)},disabled:1===s,className:"px-4 py-2 mx-2 bg-gray-200 rounded disabled:opacity-50",children:"Previous"}),(0,t.jsx)("button",{onClick:()=>{s<Math.ceil(e.length/9)&&r(s+1)},disabled:s===Math.ceil(e.length/9),className:"px-4 py-2 mx-2 bg-gray-200 rounded disabled:opacity-50",children:"Next"})]}),(0,t.jsx)(n,{isOpen:!!o,onClose:()=>{d(null)},children:o&&(0,t.jsxs)("div",{className:"text-center",children:[(0,t.jsx)("h2",{className:"text-yellow-600 text-3xl font-bold mb-4",children:o.brand_name}),(0,t.jsx)(c(),{src:o.image||i,alt:o.name,className:"w-full h-64 object-cover mb-4 rounded"}),(0,t.jsx)("h3",{className:"text-gray-800 text-2xl font-semibold mb-2",children:o.name}),(0,t.jsx)("p",{className:"text-gray-600 mb-4",children:o.description})]})})]})}},8180:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>d,metadata:()=>l});var t=r(2740),i=r(5111),a=r.n(i),n=r(5062),o=r.n(n);r(1135);var c=r(9516);let l={title:"Power-All",description:"Generated by create next app"};function d({children:e}){return(0,t.jsx)("html",{lang:"en",children:(0,t.jsx)("body",{className:`${a().variable} ${o().variable} antialiased`,children:(0,t.jsx)("div",{className:"h-screen",children:(0,t.jsxs)("div",{className:"flex flex-col h-screen overflow-hidden",children:[(0,t.jsx)("div",{className:"flex-shrink-0",children:(0,t.jsx)("header",{style:{backgroundColor:"#18181b",color:"white",boxShadow:" 0px 0px 15px 0px rgba(255, 255, 255, 0.07)",zIndex:999},children:(0,t.jsx)(c.default,{})})}),(0,t.jsx)("div",{className:"flex-grow overflow-y-auto custom-scrollbar scrollbar-gutter",children:e}),(0,t.jsx)("div",{className:"flex-shrink-0",children:(0,t.jsx)("footer",{className:"bg-zinc-900 text-white py-6  z-index: 999; ",children:(0,t.jsx)("div",{className:"container mx-auto px-4 text-center",children:(0,t.jsx)("p",{children:"\xa9 2024 PowerGen Solutions. All rights reserved."})})})})]})})})})}},5354:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>i});var t=r(2740);r(6301);let i=({children:e})=>(0,t.jsx)("div",{children:(0,t.jsx)("div",{children:e})})},9077:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>i});var t=r(2740);r(6301);let i=({children:e,search:s,products:r,...i})=>(0,t.jsxs)("div",{className:"h-full grid grid-cols-1 md:grid-cols-4 xl:grid-cols-5 ",children:[(0,t.jsx)("div",{className:"md:col-span-1  bg-gray-100 p-4  shadow-md",children:s}),(0,t.jsx)("div",{className:"md:col-span-3 xl:col-span-4 overflow-y-auto",children:r})]})},9516:(e,s,r)=>{"use strict";r.d(s,{default:()=>t});let t=(0,r(6760).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\RENDANI\\\\Documents\\\\Development\\\\generators\\\\Nextjs\\\\bigr-energy\\\\src\\\\components\\\\Header.jsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\RENDANI\\Documents\\Development\\generators\\Nextjs\\bigr-energy\\src\\components\\Header.jsx","default")},9097:(e,s,r)=>{"use strict";r.d(s,{default:()=>t});let t=(0,r(6760).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\RENDANI\\\\Documents\\\\Development\\\\generators\\\\Nextjs\\\\bigr-energy\\\\src\\\\components\\\\ProductSection.jsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\RENDANI\\Documents\\Development\\generators\\Nextjs\\bigr-energy\\src\\components\\ProductSection.jsx","default")},2062:(e,s,r)=>{"use strict";r.d(s,{_R:()=>o,A6:()=>c,gX:()=>a,Go:()=>n,oH:()=>l,zb:()=>x,dJ:()=>m,dM:()=>p,Y$:()=>d,tX:()=>A,Wu:()=>i});let t=(0,r(820).createPool)({host:process.env.DB_HOST,port:process.env.DB_PORT,user:process.env.DB_USER,password:process.env.DB_PASSWORD,database:process.env.DB_DATABASE,waitForConnections:!0,connectionLimit:10,queueLimit:0});t.on("error",e=>{e?console.log("Database pool error:",e):console.log("database connected, ",process.env.HOST)}),t.on("connection",e=>{console.log("database connected, ",process.env.DB_PORT)});let i=async()=>{try{return await t.query("SELECT * FROM services;")}catch(e){throw console.log("error",e),e}},a=async()=>{let e=`
+  SELECT DISTINCT 
+    c.id AS category_id, 
+    c.category 
+  FROM categories c
+  JOIN services_categories sc ON c.id = sc.category_id
+  JOIN services s ON sc.service_id = s.id;
+  `;try{return await t.query(e)}catch(e){throw console.log("error",e),e}},n=async e=>{let s=`
+  SELECT DISTINCT 
+    c.id AS category_id, 
+    c.category 
+  FROM categories c
+  JOIN services_categories sc ON c.id = sc.category_id
+  JOIN services s ON sc.service_id = s.id
+  WHERE s.id = ?; 
+`;try{return await t.query(s,[e])}catch(e){throw console.log("error",e),e}},o=async()=>{let e=`
+  SELECT DISTINCT 
+    b.id AS brand_id, 
+    b.brand_name, 
+    b.description 
+  FROM brands b
+  JOIN brands_services_categories bsc ON b.id = bsc.brand_id
+  JOIN services s ON bsc.service_id = s.id;
+`;try{return await t.query(e)}catch(e){throw console.log("error",e),e}},c=async e=>{let s=`
+  SELECT DISTINCT 
+    b.id AS brand_id, 
+    b.brand_name, 
+    b.description 
+  FROM brands b
+  JOIN brands_services_categories bsc ON b.id = bsc.brand_id
+  JOIN services s ON bsc.service_id = s.id
+  WHERE s.id = ?;
+`;try{return await t.query(s,[e])}catch(e){throw console.log("error",e),e}},l=async()=>{let e=`
+  SELECT 
+    p.name, 
+    p.description, 
+    p.feature,
+    b.brand_name, 
+    s.service_name, 
+    c.category
+  FROM products p
+  JOIN brands b ON p.brand_id = b.id
+  JOIN services s ON p.service_id = s.id
+  JOIN categories c ON p.category_id = c.id;
+  `;try{return await t.query(e)}catch(e){throw console.log("error",e),e}},d=async e=>{let s=`
+  SELECT 
+    p.name, 
+    p.description, 
+    p.feature,
+    b.brand_name, 
+    s.service_name, 
+    c.category
+  FROM products p
+  JOIN brands b ON p.brand_id = b.id
+  JOIN services s ON p.service_id = s.id
+  JOIN categories c ON p.category_id = c.id
+  WHERE s.id = ?;
+`;try{return await t.query(s,[e])}catch(e){throw console.log("error",e),e}},A=async e=>{let s=`
+  SELECT 
+    p.name, 
+    p.description, 
+    p.feature,
+    b.brand_name, 
+    s.service_name, 
+    c.category
+  FROM products p
+  JOIN brands b ON p.brand_id = b.id
+  JOIN services s ON p.service_id = s.id
+  JOIN categories c ON p.category_id = c.id
+  WHERE b.id = ?;
+  `;try{return await t.query(s,[e])}catch(e){throw console.log("error",e),e}},m=async e=>{let s=`
+  SELECT 
+    p.name, 
+    p.description, 
+    p.feature, 
+    b.brand_name, 
+    s.service_name
+  FROM products p
+  JOIN brands b ON p.brand_id = b.id
+  JOIN services s ON p.service_id = s.id
+  JOIN categories c ON p.category_id = c.id
+  WHERE c.id = ?
+  ORDER BY p.name;
+  `;try{return await t.query(s,[e])}catch(e){throw console.error("Error in GET_PRODUCT_BY_CATEGORY_ID:",e),e}},p=async(e,s)=>{let r=`
+  SELECT 
+    p.name, 
+    p.description, 
+    p.feature, 
+    b.brand_name, 
+    s.service_name
+  FROM products p
+  JOIN brands b ON p.brand_id = b.id
+  JOIN services s ON p.service_id = s.id
+  JOIN categories c ON p.category_id = c.id
+  WHERE s.id = ? 
+  AND c.id = ?
+  ORDER BY p.name;
+  `;try{return await t.query(r,[e,s])}catch(e){throw console.log("error",e),e}},x=async(e,s)=>{let r=`
+    SELECT 
+    p.name, 
+    p.description, 
+    p.feature, 
+    b.brand_name, 
+    s.service_name
+  FROM products p
+  JOIN brands b ON p.brand_id = b.id
+  JOIN services s ON p.service_id = s.id
+  JOIN categories c ON p.category_id = c.id
+  WHERE b.id = ? 
+  AND c.id = ?
+  ORDER BY p.name;
+  `;try{return await t.query(r,[e,s])}catch(e){throw console.log("error",e),e}}},440:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>i});var t=r(8077);let i=async e=>[{type:"image/x-icon",sizes:"16x16",url:(0,t.fillMetadataSegment)(".",await e.params,"favicon.ico")+""}]},1135:()=>{}};
