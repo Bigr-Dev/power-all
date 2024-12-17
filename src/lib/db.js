bccfb154d4aca42d85584e9c4d0e867c7a94fa75
Bigr-Dev <rendani.ramaphosa@gmail.com>
2024-12-17 10:58:50 +0200

    rollout

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,11 @@
 import { createPool } from "mysql2/promise";
 
 const pool = createPool({
-  host: process.env.DB_HOST || "154.0.175.29",
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || "bigrgtmi_api",
-  password: process.env.DB_PASSWORD || "+27815644490",
-  database: process.env.DB_DATABASE || "bigrgtmi_power",
+  host: "154.0.175.29",
+  port: 3306,
+  user: "bigrgtmi_api",
+  password: "+27815644490",
+  database: "bigrgtmi_power",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
